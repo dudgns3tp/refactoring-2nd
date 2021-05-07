7282289301eacb7d8631b01ea64533476abb20ca
younghoon.choi <c_yh0327@naver.com>
2021-05-07 11:27:33 +0900

    html 렌더

--- src/ch1/1-1/1.1.js
+++ src/ch1/1-1/1.1.js
@@ -30,4 +30,30 @@ function renderPlainText(data) {
     }
 }
 
-console.log(statement(invoice, plays));
+function htmlStatement(invoce, plays) {
+    return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+    let result = `<h1> 청구 내역 (고객명: ${data.customer}) </h1> \n`;
+    result += '<table>\n';
+    result += '<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>';
+    for (let perf of data.performances) {
+        result += `<tr><td>${perf.play.name}</tr><td>(${perf.audience})석</td>`;
+        result += `<td> ${usd(perf.amount)}</td></tr>\n`;
+    }
+    result += '</table>\n';
+    result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>적립 포인트: <em> ${data.totalVolumeCredits}점</em></p> \n`;
+    return result;
+
+    function usd(aNumber) {
+        return new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD',
+            minimunFractionDigits: 2,
+        }).format(aNumber / 100);
+    }
+}
+
+console.log(htmlStatement(invoice, plays));
---
+++ src/ch1/1-1/statement.js
@@ -0,0 +1,3 @@
+const createStatementData = require('./createStatementData');
+const invoice = require('./invoices.json');
+const plays = require('./plays.json');
