1e7fbaa8563c941061d0d3d3c050297fc2dbf0c6
younghoon.choi <c_yh0327@naver.com>
2021-05-07 09:35:07 +0900

    1장: totalVolumeCredits 쪼개기

--- src/ch1/1-1/1.1.js
+++ src/ch1/1-1/1.1.js
@@ -9,17 +9,22 @@ const plays = require('./plays.json');
 
 function statement(invoice, plays) {
     let totalAmount = 0;
-    let volumeCredits = 0;
     let result = `청구 내역 (고객명: ${invoice.customer}) \n`;
     for (let perf of invoice.performances) {
-        //포인트를 적립한다.
-        volumeCredits += volumeCreditsFor(perf);
         result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
         totalAmount += amountFor(perf);
     }
     result += `총액: ${usd(totalAmount)}\n`;
-    result += `적립 포인트: ${volumeCredits}점 \n`;
+    result += `적립 포인트: ${totalVolumeCredits()}점 \n`;
     return result;
+}
+
+function totalVolumeCredits() {
+    let volumeCredits = 0;
+    for (let perf of invoice.performances) {
+        volumeCredits += volumeCreditsFor(perf);
+    }
+    return volumeCredits;
 }
 
 function usd(aNumber) {
