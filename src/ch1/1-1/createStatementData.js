2e9977a36553d1ea8681ac96c36687f6a879a43e
younghoon.choi <c_yh0327@naver.com>
2021-05-07 11:49:40 +0900

    적립 포인트 계산하기도 클래스에 추가

--- src/ch1/1-1/createStatementData.js
+++ src/ch1/1-1/createStatementData.js
@@ -6,7 +6,7 @@ class PerformanceCalculator {
 
     get amount() {
         let result = 0;
-        switch (this.performance.play.type) {
+        switch (this.play.type) {
             case 'tragedy': //비극
                 result = 40000;
                 if (this.performance.audience > 30) {
@@ -23,6 +23,13 @@ class PerformanceCalculator {
             default:
                 throw new Error(`알 수 없는 장르: ${this.play.type}`);
         }
+        return result;
+    }
+
+    get volumeCredits() {
+        let result = 0;
+        result += Math.max(this.performance.audience - 30, 0);
+        if ('comedy' === this.play.type) result += Math.floor(this.performance.audience / 5);
         return result;
     }
 }
@@ -47,8 +54,8 @@ const createStatementData = function (invoice, plays) {
         const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance);
         result.play = calculator.play;
-        result.amount = amountFor(result);
-        result.volumeCredits = volumeCreditsFor(result);
+        result.amount = calculator.amount;
+        result.volumeCredits = calculator.volumeCredits;
         return result;
     }
 
