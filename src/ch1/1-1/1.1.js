6d4d33b5ec0197f1c4a8a4e6ccf78dff64d7d515
younghoon.choi <c_yh0327@naver.com>
2021-05-07 09:47:12 +0900

    1장: 계산단계 포맷팅 단계 분리

--- src/ch1/1-1/1.1.js
+++ src/ch1/1-1/1.1.js
@@ -7,7 +7,7 @@
 const invoice = require('./invoices.json');
 const plays = require('./plays.json');
 
-function statement(invoice, plays) {
+function renderPlainText(invoice, plays) {
     let result = `청구 내역 (고객명: ${invoice.customer}) \n`;
     for (let perf of invoice.performances) {
         result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
@@ -15,63 +15,68 @@ function statement(invoice, plays) {
     result += `총액: ${usd(totalAmount())}\n`;
     result += `적립 포인트: ${totalVolumeCredits()}점 \n`;
     return result;
-}
 
-function totalAmount() {
-    let result = 0;
-    for (let perf of invoice.performances) {
-        result += amountFor(perf);
+    function totalAmount() {
+        let result = 0;
+        for (let perf of invoice.performances) {
+            result += amountFor(perf);
+        }
+        return result;
     }
-    return result;
-}
 
-function totalVolumeCredits() {
-    let result = 0;
-    for (let perf of invoice.performances) {
-        result += volumeCreditsFor(perf);
+    function totalVolumeCredits() {
+        let result = 0;
+        for (let perf of invoice.performances) {
+            result += volumeCreditsFor(perf);
+        }
+        return result;
     }
-    return result;
-}
 
-function usd(aNumber) {
-    return new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD',
-        minimunFractionDigits: 2,
-    }).format(aNumber / 100);
-}
-
-function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ('comedy' === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
-    return result;
-}
-
-function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-}
-
-function amountFor(aPerformance) {
-    let result = 0;
-    switch (playFor(aPerformance).type) {
-        case 'tragedy': //비극
-            result = 40000;
-            if (aPerformance.audience > 30) {
-                result += 1000 * (aPerformance.audience - 30);
-            }
-            break;
-        case 'comedy':
-            result = 30000;
-            if (aPerformance.audience > 20) {
-                result += 10000 + 500 * (aPerformance.audience - 20);
-            }
-            result += 300 * aPerformance.audience;
-            break;
-        default:
-            throw new Error(`알 수 없는 장르: ${playFor(aPerformance)}`);
+    function usd(aNumber) {
+        return new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD',
+            minimunFractionDigits: 2,
+        }).format(aNumber / 100);
     }
-    return result;
+
+    function volumeCreditsFor(aPerformance) {
+        let result = 0;
+        result += Math.max(aPerformance.audience - 30, 0);
+        if ('comedy' === playFor(aPerformance).type)
+            result += Math.floor(aPerformance.audience / 5);
+        return result;
+    }
+
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID];
+    }
+
+    function amountFor(aPerformance) {
+        let result = 0;
+        switch (playFor(aPerformance).type) {
+            case 'tragedy': //비극
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
+                }
+                break;
+            case 'comedy':
+                result = 30000;
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience;
+                break;
+            default:
+                throw new Error(`알 수 없는 장르: ${playFor(aPerformance)}`);
+        }
+        return result;
+    }
+}
+
+function statement(invoice, plays) {
+    return renderPlainText(invoice, plays);
 }
 
 console.log(statement(invoice, plays));
