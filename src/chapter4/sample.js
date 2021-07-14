560a0c46cce686b062985b1ef3b68331c9512138
최영훈 <c_yh0327@naver.com>
2021-07-14 20:25:08 +0900

    test init

---
+++ src/chapter4/sample.js
@@ -0,0 +1,16 @@
+/**
+ *
+ */
+
+function sampleProvinceData() {
+    return {
+        name: 'Asia',
+        producers: [
+            { name: 'Byzantium', const: 10, production: 9 },
+            { name: 'Attalia', const: 12, production: 10 },
+            { name: 'Sinope', const: 10, production: 6 },
+        ],
+        demand: 30,
+        price: 20,
+    };
+}
