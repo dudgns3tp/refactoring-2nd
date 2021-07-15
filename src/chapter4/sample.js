cfa11e7898fa23b4a516b3428c33d14364edf781
최영훈 <c_yh0327@naver.com>
2021-07-15 23:06:17 +0900

    const -> cost 오타 수정

--- src/chapter4/sample.js
+++ src/chapter4/sample.js
@@ -2,9 +2,9 @@ function sampleProvinceData() {
     return {
         name: 'Asia',
         producers: [
-            { name: 'Byzantium', const: 10, production: 9 },
-            { name: 'Attalia', const: 12, production: 10 },
-            { name: 'Sinope', const: 10, production: 6 },
+            { name: 'Byzantium', cost: 10, production: 9 },
+            { name: 'Attalia', cost: 12, production: 10 },
+            { name: 'Sinope', cost: 10, production: 6 },
         ],
         demand: 30,
         price: 20,
